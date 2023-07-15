314afa5d561dd34c0b6abb29e153a4d0bc632436
Eugene Egorov <egorovzhenya@gmail.com>
2023-07-15 21:15:46 +0400

    Update test fixtures for plain format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,27 +6,27 @@ from gendiff.scripts.generate_diff import generate_diff
 FIXTURES_DIR = Path("tests/fixtures")
 
 
-def build_test_case(file1, file2):
+def build_test_case(file1, file2, format):
 	file1_name = Path(file1).stem
 	file2_name = Path(file2).stem
 
-	with open(FIXTURES_DIR / f"diff_{file1_name}_{file2_name}.txt") as file:
+	with open(FIXTURES_DIR / f"diff_{file1_name}_{file2_name}_{format}.txt") as file:
 		diff = file.read().strip()
 
-		return (FIXTURES_DIR / f"{file1}", FIXTURES_DIR / f"{file2}", diff)
+		return (FIXTURES_DIR / f"{file1}", FIXTURES_DIR / f"{file2}", format, diff)
 
 
 cases = [
-	build_test_case(f1, f2) for f1, f2 in [
-		["file1.json", "file2.json"],
-		["file3.yml", "file4.yml"],
-		["file5.json", "file6.json"],
-		["file7.yml", "file8.yml"],
+	build_test_case(f1, f2, format) for f1, f2, format in [
+		["file1.json", "file2.json", "stylish"],
+		["file3.yml", "file4.yml", "stylish"],
+		["file1.json", "file2.json", "plain"],
+		["file3.yml", "file4.yml", "plain"],
 	]
 ]
 
 
-@pytest.mark.parametrize("path1, path2, expected", cases)
-def test_gendiff(path1, path2, expected):
-    result = generate_diff(path1, path2, "stylish")
+@pytest.mark.parametrize("path1, path2, format, expected", cases)
+def test_gendiff(path1, path2, format, expected):
+    result = generate_diff(path1, path2, format)
     assert result == expected
